80f4c86f2c3be0c5682a1fefa6292b0487ff4137
Amar Dhadbale <amardhadbale@gmail.com>
2026-01-03 22:04:05 +0530

    Day 1 & 2 added

---
+++ Day2/App.js
@@ -0,0 +1,31 @@
+//GET THE NO OF NOTES
+
+
+let amount = 25455;
+let number;
+
+if (amount>=500) {
+     number = (Math.floor(amount/500));
+     amount = amount%500;
+    console.log(number);
+}
+if (amount>=200) {
+    number = (Math.floor(amount/200));
+    amount = amount%200;
+    console.log(number);
+}
+if (amount>=100) {
+    number = (Math.floor(amount/100));
+    amount = amount%100;
+    console.log(number);
+}
+if(amount>=50){
+     number = (Math.floor(amount/50));
+    amount = amount%50;
+    console.log(number);
+}
+if (amount>=1) {
+    number = (Math.floor(amount/1));
+    amount = amount%1;
+    console.log(number);
+}
---
+++ Day2/ternary_operator.js
@@ -0,0 +1,3 @@
+let num = 0;
+console.log(num>0?"hii":"hello");
+console.log(num>0?"Positive":num<0?"Negative":"Num is zero")
