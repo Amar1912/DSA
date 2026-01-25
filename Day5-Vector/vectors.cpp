cab4a1a43c7381c1a6b252343213aeb616a1c904
Amar Dhadbale <amardhadbale@gmail.com>
2026-01-25 22:29:37 +0530

    Vectors

---
+++ Day5-Vector/vectors.cpp
@@ -0,0 +1,27 @@
+//Binary Search :- O(logn)
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    
+ 
+    vector<int>vec = {1,2,3,4,5,6,7};
+    vector<int>vec2;
+
+    vec2.push_back(25);
+
+    int sz = vec.size();
+    int sz2 = vec2.size();
+
+    cout<< vec.front();
+    // for (int i: vec)
+    // {
+    //     cout<< i <<endl;
+    // }
+    
+
+    return 0;
+}
