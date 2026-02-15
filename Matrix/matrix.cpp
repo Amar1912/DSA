48d0dd491712c3ba172d15d997a655d58df50948
Amar Dhadbale <amardhadbale@gmail.com>
2026-02-15 10:17:57 +0530

    matrics in cpp

---
+++ Matrix/matrix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //int matrix[4][3];
+    int matrix[4][3] ={{1,2,3},{2,3,4},{3,4,5},{1,2,3}};
+    int row = 4;
+    int cols = 3;
+
+    cout<< matrix[2][1];
+
+    return 0;
+}
